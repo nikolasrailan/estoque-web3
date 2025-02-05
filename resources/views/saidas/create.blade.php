66baac2a4aa43e858f5016384896f0cc43d14bea
Nikolas <nikolasrailanmp@gmail.com>
2025-02-05 11:27:20 -0300

    index saida

--- resources/views/saidas/create.blade.php
+++ resources/views/saidas/create.blade.php
@@ -1,38 +1,31 @@
-@extends('layouts.app')
+<head>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<x-app-layout>
+  <x-slot name="header">
+  <div class="d-flex justify-content-between align-items-center">
+       <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-900 leading-tight">
+          {{ __('Registrar Saida do Estoque ') }}
+      </h2>
+          <a href="/saidas"><button class="btn btn-primary btn-lg">Listar Saidas</button></a>
+</div>
+  </x-slot>
 
-@section('content')
-<h2>Registrar Saída de Estoque</h2>
-
-@if ($errors->any())
-    <div>
-        <strong>Erro!</strong> Verifique os campos abaixo.
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-
-<form action="{{ route('saidas.store') }}" method="POST">
-    @csrf
-    <label>Cliente:</label>
-    <select name="cliente_id" required>
-        @foreach($clientes as $cliente)
-            <option value="{{ $cliente->id }}">{{ $cliente->nome }}</option>
-        @endforeach
-    </select>
-
-    <label>Produto:</label>
-    <select name="produto_id" required>
-        @foreach($produtos as $produto)
-            <option value="{{ $produto->id }}">{{ $produto->nome }} ({{ $produto->estoque }} disponíveis)</option>
-        @endforeach
-    </select>
-
-    <label>Quantidade:</label>
-    <input type="number" name="quantidade" required min="1">
-
-    <button type="submit">Registrar</button>
-</form>
-@endsection
+  <div class="py-12">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+          <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg" style="border:solid 1px black;">
+              <div class="p-6 text-gray-900 dark:text-gray-900">
+              <div class="form-container">
+              <form action="{{ route('saidas.store') }}" method="POST" class="container mt-4">
+                  @csrf
+              <div style="padding:15px">
+                      <h3 class="text-center font-weight-bold mt-4 mb-4">Saida</h3>
+                      
+                      
+                  </form>
+              </div> 
+              </div>
+          </div>
+      </div>
+  </div>
+</x-app-layout>
